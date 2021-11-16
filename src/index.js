2a2ea54749d414bc6265383120072b15df01181e
ericpkatz <ericpkatz@gmail.com>
2021-11-15 19:48:03 -0500

    commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const faker = require('faker');
 const generateNumbers = require('./generator');
-console.log(faker.vehicle.model());
+//console.log(faker.vehicle.model());
 
 const numbers = generateNumbers(11);
 
 const ul = document.querySelector('#numbers-list');
+
+let chosenNumber = window.location.hash.slice(1) * 1;
 
 const render = ()=> {
   const html = numbers.map( num => {
@@ -13,10 +15,18 @@ const render = ()=> {
         <a href='#${ num }'>
         ${ num }
         </a>
+        ${
+          chosenNumber === num ? `<div>${ num * 7}</div>`: ''
+        }
       </li>
     `;
   }).join('');
   ul.innerHTML = html;
 };
+
+window.addEventListener('hashchange', ()=> {
+  chosenNumber = window.location.hash.slice(1) * 1;
+  render();
+});
 
 render();
